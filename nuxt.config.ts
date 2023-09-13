264a475ee152aa6cbf221bbac544d0254f4f9b6b
stanfodgit <khomych.other@gmail.com>
2023-09-14 00:52:24 +0300

    create project

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ["@/assets/css/global.scss"],
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  additionalData: `@import "@/assets/css/utils/_variables.scss";`,
+              },
+          },
+      },
+  }
+})
